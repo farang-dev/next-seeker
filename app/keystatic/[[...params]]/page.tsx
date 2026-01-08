d12db02002de609e34a7facf3cecde5a85dcdadc
Masafumi Nozawa <masafumi.nozawa.q@gmail.com>
2026-01-08 15:59:25 +0100

    fix: keystatic config import path

--- app/keystatic/[[...params]]/page.tsx
+++ app/keystatic/[[...params]]/page.tsx
@@ -1,4 +1,5 @@
-import { makePage } from '@keystatic/next/ui/app';
-import config from '@/keystatic.config';
+import KeystaticPage from '@/components/keystatic/keystatic-page';
 
-export default makePage(config);
+export default function Page() {
+    return <KeystaticPage />;
+}
